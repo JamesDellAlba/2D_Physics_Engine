10ff598eaaba46b1ab333cc86f83cdf403f00f09
JamesDellAlba <jpdellalba@gmail.com>
2020-07-29 11:25:30 -0400

    beginning of experimentation

--- Source.cpp
+++ Source.cpp
@@ -19,7 +19,7 @@ const char *fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
 "void main()\n"
 "{\n"
-"FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);\n"
+"FragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);\n"
 "}\n";
 
 int main() {
