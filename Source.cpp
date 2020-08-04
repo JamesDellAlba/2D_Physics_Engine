ceb5470f6e517c47414900acce1d9203eff7a681
JamesDellAlba <jpdellalba@gmail.com>
2020-08-04 14:48:58 -0400

    rainbow triangle oooo

--- Source.cpp
+++ Source.cpp
@@ -9,20 +9,21 @@ void processInput(GLFWwindow *window);
 //shader source code in GLSL
 const char *vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
-"out vec4 vertexColor;\n"
+"layout (location = 1) in vec3 aColor;\n"
+"out vec3 ourColor;\n"
 "void main()\n"
 "{\n"
-"   gl_Position = vec4(aPos, 1.0);\n"
-"	vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
-"}\0";
+"	gl_Position = vec4(aPos, 1.0);\n"
+"	ourColor = aColor;\n" // set ourColor to the input color we got from the vertex data
+"}\n\0";
 
 //fragment shader source code in GLSL
 const char *fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
-"uniform vec4 ourColor;\n"
+"in vec3 ourColor;\n"
 "void main()\n"
 "{\n"
-"	FragColor = ourColor;\n"
+"	FragColor = vec4(ourColor, 1.0);\n"
 "}\n\0";
 
 int main() {
@@ -58,9 +59,10 @@ int main() {
 
 	//set verticies of the triangle
 	float vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f,
-		0.0f,  0.5f, 0.0f
+		// positions         // colors
+		 0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
+		-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
+		 0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
 	};
 
 	//create a shader object (referenced by an ID)
@@ -119,16 +121,20 @@ int main() {
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	// 3. then set our vertex attributes pointers
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+	// color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 	//render loop
 	int i = 0;
 	while (!glfwWindowShouldClose(window)) {
 		processInput(window);
 		
-		cout << "rendering frame " << i << endl;
-		i++; //count each frame
+		//cout << "rendering frame " << i << endl;
+		//i++; //count each frame
 
 		// --- Drawing code (in render loop) ---
 		glUseProgram(shaderProgram);
